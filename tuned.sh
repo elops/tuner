7e1c706b33d095686fe21f9a3cfeec55684309d7
Hrvoje Spoljar <hrvoje.spoljar@gmail.com>
2014-10-29 13:52:47 +0100

    procfs sysctl stuff almost there

--- tuned.sh
+++ tuned.sh
@@ -3,6 +3,7 @@
 # PATH
 
 # defaults
+low_memory_limit=12582912     # consider systems below or equal to this mem a low mem system
 lock_dir_created=0
 lock_dir='/tmp/being-tuned'
 hypervisor=0
@@ -36,6 +37,26 @@ _generic_err() {
   _unlock
   kill -SIGPIPE $$      #  nail the original shell if we were invoked in subshell
   exit "$rc"
+}
+
+sysctl_set() {
+  setting="$1"
+  value="$2"
+
+  if [ "$(sysctl -n "$setting")" -ne "$value" ]; then
+    # set new value
+    sysctl -w "$setting=$value"
+    # update sysctl.conf if necessary
+    if egrep -q "^\s*$setting\ " /etc/sysctl.conf; then
+      sed -i.bak -e "s/^\s*$setting.*/# updated by site5-tuned.sh script on $(date)\n$setting = $value/" /etc/sysctl.conf 
+    else
+      echo -en "# set by site5-tuned.sh script on $(date)\n$setting = $value\n" >> /etc/sysctl.conf
+    fi
+  fi
+}
+
+debug() {
+  echo "$@" 1>2
 }
 
 ## script starts here
@@ -82,4 +103,52 @@ do
   echo ">> $device"
 done
 
+## Making a change...
 
+## Generic settings
+# reduce swap usage; less paging in and paging out will save us some IO
+# default : 60
+sysctl_set vm.swappiness 10
+
+# be more inclined to *not* moving long lived processes from cpu to cpu 
+# which will provide more use of local CPU cache
+# default : 500000
+sysctl_set kernel.sched_migration_cost 5000000
+
+# The wake-up preemption granularity. Increasing this variable reduces wake-up
+# preemption, reducing disturbance of compute bound tasks.
+# default : 3000000
+sysctl_set kernel.sched_min_granularity_ns 10000000    
+# default : 4000000
+sysctl_set kernel.sched_wakeup_granularity_ns 15000000
+# default : 24000000
+sysctl_set kernel.sched_latency_ns 24000000
+
+## changed that depend on system profile
+# if <= 12 GB RAM 
+#  vm.vfs_cache_pressure = 200       # (don't let SLABS eat little RAM we have)
+#  vm.dirty_ratio = 10               # don't allow large writes to grow in mem, flush them earlier and +make sure writeback throttling is activated as early as possible
+#  vm.dirty_background_ratio = 3 
+#  vm.overcommit_memory = 2          # strict memory accounting; we wont allow memory overuse to avoid memory hogs when under pressure
+#  vm.overcommit_ratio = 200         # based on observation this ratio is sane ceil value which can be allowed without experiencing mem hogs
+
+if [ "$total_memory" -le "$low_memory_system" ]; then
+  debug "Detected $total_memory which is based on our config low, applying low mem environment tunables"
+  debug " * Setting vfs_cache_pressure to higher value to spare some mem which is sitting unused in slabs"
+  sysctl_set vm.vfs_cache_pressure 200
+  debug " * Tuning dirty ratios to make better use of writeback throttling and give us more latency during high disk IO"
+  sysctl_set vm.dirty_ratio 10
+  sysctl_set vm.dirty_background_ratio 3
+  debug " * Prevent overcommiting memory; set higher limit and impose strict accounting"
+  sysctl_set vm.overcommit_ratio 200
+  sysctl_set vm.overcommit_memory 2
+fi
+
+# TODO
+# Apache limits
+# devise how much RAM single process is using
+# (total RAM - 2 GB / avg process usage) * 1.1 to allow for some slack room
+# ensure requests per sec are below half of what we want to set as limit; else report
+
+# TODO
+# sysfs settings for disk scheduler
